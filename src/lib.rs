697b29752496ee2951f1e2375655a0c7dca4bdd6
benjamin giudice <ben.giudice@gmail.com>
2023-01-10 15:10:26 +0800

    Implement search for run

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
