9856792fd6f9371162229db753af5be3ea400945
benjamin giudice <ben.giudice@gmail.com>
2023-01-10 14:40:53 +0800

    Implement result error handling for run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::{env, fs, process};
 
 fn main() {
@@ -15,10 +16,12 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(&config.file_path).expect("Error while reading file.");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
