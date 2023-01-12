2a623b2cbb9501cb8ffc328deac8d0ca0bf83dc7
Benjamin Giudice <ben.giudice@gmail.com>
2023-01-12 23:41:34 +0800

    Reduce vertical space

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,22 @@
 use std::{env, error::Error, fs};
-
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
-
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
     } else {
         search(&config.query, &contents)
     };
-
     for line in results {
         println!("{line}");
     }
-
     Ok(())
 }
-
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
         .filter(|line| line.contains(query))
         .collect()
 }
-
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase(); //TODO: Handle Unicode
     contents
@@ -30,31 +24,24 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
         .filter(|line| line.to_lowercase().contains(&query))
         .collect()
 }
-
 pub struct Config {
     pub query: String,
     pub file_path: String,
     pub ignore_case: bool,
 }
-
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         //TODO: handle panic on invalid unicode
-
         args.next();
-
         let query = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
         };
-
         let file_path = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
         };
-
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-
         Ok(Config {
             query,
             file_path,
@@ -62,11 +49,9 @@ impl Config {
         })
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
@@ -75,13 +60,11 @@ Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
-
         assert_eq!(
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
     }
-
     #[test]
     fn case_sensitive() {
         let query = "duct";
@@ -90,7 +73,6 @@ Rust:
 safe, fast, productive.
 Pick three.
 Duct tape.";
-
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use std::{env, process};
-
 use rgrep::Config;
-
+use std::{env, process};
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
     if let Err(e) = rgrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
